d5cbaee4da46853bc0962e3ed93e7b3d4a176e4e
Konstantin Burykin <burkostya@gmail.com>
2014-02-26 01:44:02 +0400

    WIP

--- test/map-merge.s.js
+++ test/map-merge.s.js
@@ -1,22 +1,80 @@
 var expect = require('chai').expect;
+var numbers = require('stream-spectrum/readable/number');
 
-var stream = require('../lib/map\-merge.s');
+var merge = require('../lib/map-merge.s');
 
-describe('map\-merge.s', function () {
-  it('should stream', function (done) {
+describe('map-merge.s', function () {
+  it('should merge stream with map function', function (done) {
     var results = [];
-    var instance = stream();
-    instance.on('readable', function () {
+    var sum = merge(function (first, second, done) {
+      done(null, first + second);
+    });
+    var oneToFive = numbers({ from: 1, to: 5  });
+    var sixToTen  = numbers({ from: 6, to: 10 });
+    sum.on('readable', function () {
       var item;
-      while ((item = instance.read()) !== null) {
+      while ((item = sum.read()) !== null) {
         results.push(item);
       }
     });
-    instance.on('end', function () {
-      expect(results).to.have.length(1);
+    sum.on('end', function () {
+      expect(results).to.have.length(5);
+      expect(results[0]).to.equal(7);
+      expect(results[1]).to.equal(9);
+      expect(results[2]).to.equal(11);
+      expect(results[3]).to.equal(13);
+      expect(results[4]).to.equal(15);
       done();
     });
-    instance.write('some');
-    instance.end();
+    oneToFive.pipe(sum);
+    sixToTen.pipe(sum);
+  });
+  it('should drop packets from longer stream by default', function (done) {
+    var results = [];
+    var sum = merge(function (first, second, done) {
+      done(null, first + second);
+    });
+    var oneToFive = numbers({ from: 1, to: 5  });
+    var sixToTen  = numbers({ from: 6, to: 8 });
+    sum.on('readable', function () {
+      var item;
+      while ((item = sum.read()) !== null) {
+        results.push(item);
+      }
+    });
+    sum.on('end', function () {
+      expect(results).to.have.length(3);
+      expect(results[0]).to.equal(7);
+      expect(results[1]).to.equal(9);
+      expect(results[2]).to.equal(11);
+      done();
+    });
+    oneToFive.pipe(sum);
+    sixToTen.pipe(sum);
+  });
+  it('should not drop packets from longer stream with if option present', function (done) {
+    var results = [];
+    var sum = merge({ dropTail: false }, function (first, second, done) {
+      done(null, first + second);
+    });
+    var oneToFive = numbers({ from: 1, to: 5  });
+    var sixToTen  = numbers({ from: 6, to: 8 });
+    sum.on('readable', function () {
+      var item;
+      while ((item = sum.read()) !== null) {
+        results.push(item);
+      }
+    });
+    sum.on('end', function () {
+      expect(results).to.have.length(5);
+      expect(results[0]).to.equal(7);
+      expect(results[1]).to.equal(9);
+      expect(results[2]).to.equal(11);
+      expect(results[3]).to.equal(9);
+      expect(results[4]).to.equal(10);
+      done();
+    });
+    oneToFive.pipe(sum);
+    sixToTen.pipe(sum);
   });
 });
