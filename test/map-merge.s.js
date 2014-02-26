c48cadf583e6833cc323ff49daa65c46a7c30d0a
Konstantin Burykin <burkostya@gmail.com>
2014-02-27 02:26:58 +0400

    complete draft

--- test/map-merge.s.js
+++ test/map-merge.s.js
@@ -9,8 +9,8 @@ describe('map-merge.s', function () {
     var sum = merge(function (first, second, done) {
       done(null, first + second);
     });
-    var oneToFive = numbers({ from: 1, to: 5  });
-    var sixToTen  = numbers({ from: 6, to: 10 });
+    var oneToFive = numbers({ from: 1, to: 5,  objectMode: true });
+    var sixToTen  = numbers({ from: 6, to: 10, objectMode: true });
     sum.on('readable', function () {
       var item;
       while ((item = sum.read()) !== null) {
@@ -29,13 +29,13 @@ describe('map-merge.s', function () {
     oneToFive.pipe(sum);
     sixToTen.pipe(sum);
   });
-  it('should drop packets from longer stream by default', function (done) {
+  it('should drop packets from longer stream', function (done) {
     var results = [];
     var sum = merge(function (first, second, done) {
       done(null, first + second);
     });
-    var oneToFive = numbers({ from: 1, to: 5  });
-    var sixToTen  = numbers({ from: 6, to: 8 });
+    var oneToFive = numbers({ from: 1, to: 5, objectMode: true });
+    var sixToTen  = numbers({ from: 6, to: 8, objectMode: true });
     sum.on('readable', function () {
       var item;
       while ((item = sum.read()) !== null) {
@@ -47,31 +47,6 @@ describe('map-merge.s', function () {
       expect(results[0]).to.equal(7);
       expect(results[1]).to.equal(9);
       expect(results[2]).to.equal(11);
-      done();
-    });
-    oneToFive.pipe(sum);
-    sixToTen.pipe(sum);
-  });
-  it('should not drop packets from longer stream with if option present', function (done) {
-    var results = [];
-    var sum = merge({ dropTail: false }, function (first, second, done) {
-      done(null, first + second);
-    });
-    var oneToFive = numbers({ from: 1, to: 5  });
-    var sixToTen  = numbers({ from: 6, to: 8 });
-    sum.on('readable', function () {
-      var item;
-      while ((item = sum.read()) !== null) {
-        results.push(item);
-      }
-    });
-    sum.on('end', function () {
-      expect(results).to.have.length(5);
-      expect(results[0]).to.equal(7);
-      expect(results[1]).to.equal(9);
-      expect(results[2]).to.equal(11);
-      expect(results[3]).to.equal(9);
-      expect(results[4]).to.equal(10);
       done();
     });
     oneToFive.pipe(sum);
